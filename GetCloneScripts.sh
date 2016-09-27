2e140b7524e1ed52bf2b767643d86c320a1cb767
Hung-Hsin <hunghsinwu@gmail.com>
2016-09-27 12:35:45 -0700

    Modify for linux

--- GetCloneScripts.sh
+++ GetCloneScripts.sh
@@ -1,9 +1,9 @@
-git clone https://github.com/Rip-Rip/clang_complete bundle/
-git clone https://github.com/vim-scripts/genutils bundle/
-git clone https://github.com/llinfeng/mru.vim /mru/ bundle/
-git clone https://github.com/wesleyche/SrcExpl bundle/
-git clone https://github.com/vim-scripts/SuperTab bundle/
-git clone https://github.com/vim-scripts/taglist.vim bundle/ 
-git clone https://github.com/vim-scripts/tComment bundle/
-git clone https://github.com/vim-scripts/NERD bundle/
-git clone https://github.com/wesQ3/vim-windowswap bundle/
+git clone https://github.com/Rip-Rip/clang_complete 
+git clone https://github.com/vim-scripts/genutils 
+git clone https://github.com/llinfeng/mru.vim /mru/ 
+git clone https://github.com/wesleyche/SrcExpl 
+git clone https://github.com/vim-scripts/SuperTab 
+git clone https://github.com/vim-scripts/taglist.vim  
+git clone https://github.com/vim-scripts/tComment 
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/wesQ3/vim-windowswap 
---
+++ readme.txt
@@ -0,0 +1,6 @@
+On Cygwin:
+    - copy the bundle and .vimrc to $HOME/
+
+On Linux
+    - copy the bundle under /usr/share/vim/vim74/
+    - copy the .vimrc under /usr/share/vim/vim74
