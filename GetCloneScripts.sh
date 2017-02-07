360b8c7eb52e5e8b80de2b29d120fc42d50caa6c
hunghsinwu <hunghsinwu@gmail.com>
2017-02-07 11:02:37 -0800

    Update GetCloneScripts.sh

--- GetCloneScripts.sh
+++ GetCloneScripts.sh
@@ -1,10 +1,10 @@
-git clone https://github.com/Rip-Rip/clang_complete 
-git clone https://github.com/vim-scripts/genutils 
-git clone https://github.com/llinfeng/mru.vim /mru/ 
-git clone https://github.com/wesleyche/SrcExpl 
-git clone https://github.com/vim-scripts/SuperTab 
-git clone https://github.com/vim-scripts/taglist.vim  
-git clone https://github.com/vim-scripts/tComment 
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/wesQ3/vim-windowswap 
-git clone https://github.com/MattesGroeger/vim-bookmarks
+sudo git clone https://github.com/Rip-Rip/clang_complete 
+sudo git clone https://github.com/vim-scripts/genutils 
+sudo git clone https://github.com/llinfeng/mru.vim /mru/ 
+sudo git clone https://github.com/wesleyche/SrcExpl 
+sudo git clone https://github.com/vim-scripts/SuperTab 
+sudo git clone https://github.com/vim-scripts/taglist.vim  
+sudo git clone https://github.com/vim-scripts/tComment 
+sudo git clone https://github.com/scrooloose/nerdtree.git
+sudo git clone https://github.com/wesQ3/vim-windowswap 
+sudo git clone https://github.com/MattesGroeger/vim-bookmarks
