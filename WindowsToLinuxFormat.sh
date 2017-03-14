b624da6daa4f2f45ef7b7c07be8fdc3c4fdeef96
hunghsinwu <hunghsinwu@gmail.com>
2017-03-14 10:58:11 -0700

    Update WindowsToLinuxFormat.sh

--- WindowsToLinuxFormat.sh
+++ WindowsToLinuxFormat.sh
@@ -1,4 +1,4 @@
- find -type f -name '*' -exec sed -i 's/^M//g' {} \;
+find -type f -name '*' -exec sed -i 's/^M//g' {} \;
 
 find ./ -type f -exec dos2unix {} \;
 
@@ -6,4 +6,4 @@ ctags -R *
 
 ctags -R --c++-kinds=+p --fields=+iaS --extra=+q .
 
-cscope -R (the '-R' makes Cscope parse all subdirectories, not just the current directory). 
+cscope -R -b (the '-R' makes Cscope parse all subdirectories, not just the current directory).  '-b' flag (which tells Cscope to just build the database, then exit
