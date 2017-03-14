d035dedc44fc9874a9cba3475d45cbda0689d7c8
hunghsinwu <hunghsinwu@gmail.com>
2017-03-14 10:55:04 -0700

    Update WindowsToLinuxFormat.sh

--- WindowsToLinuxFormat.sh
+++ WindowsToLinuxFormat.sh
@@ -1,7 +1,9 @@
-find -type f -name '*' -exec sed -i 's/^M//g' {} \;
+ find -type f -name '*' -exec sed -i 's/^M//g' {} \;
 
 find ./ -type f -exec dos2unix {} \;
 
 ctags -R *
 
 ctags -R --c++-kinds=+p --fields=+iaS --extra=+q .
+
+cscope -R (the '-R' makes Cscope parse all subdirectories, not just the current directory). 
